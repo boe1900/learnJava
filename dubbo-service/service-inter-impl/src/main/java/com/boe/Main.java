4fce203c7f7aafce771628f236af78fb6e5a87c1
huabo <huabo1900@163.com>
2017-05-16 11:03:42 +0800

    代码结构修改

--- dubbo-service/service-inter-impl/src/main/java/com/boe/Main.java
+++ dubbo-service/service-inter-impl/src/main/java/com/boe/Main.java
@@ -7,10 +7,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "classpath:spring/applicationContext.xml" });
-        context.start();
-        System.out.println("启动成功");
-        System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟
+        new com.alibaba.dubbo.container.Main().main(args);
     }
 }
--- dubbo-service/service-inter-impl/src/main/java/com/boe/mapper/UserMapper.java
+++ dubbo-service/service-inter-impl/src/main/java/com/boe/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.boe.mapper;
 
-import com.baomidou.mybatisplus.mapper.AutoMapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.boe.service.inter.domain.User;
 
 /**
@@ -8,7 +9,7 @@ import com.boe.service.inter.domain.User;
  * User 表数据库控制层接口
  *
  */
-public interface UserMapper extends AutoMapper<User> {
+public interface UserMapper extends BaseMapper<User> {
 
 
 }
